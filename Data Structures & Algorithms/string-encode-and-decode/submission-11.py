de6e071c0393c782a30ae105039b72134ec46bc8
Gavinfornever <100469530+Gavinfornever@users.noreply.github.com>
2026-06-17 21:13:24 -0700

    Add: string-encode-and-decode - submission-11

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-11.py
@@ -0,0 +1,7 @@
+import ast
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        return str(strs)
+    def decode(self, s: str) -> List[str]:
+        return ast.literal_eval(s)
