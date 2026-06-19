90837abad44164fde0821b45ff91b4d6de79ceb8
Gavinfornever <100469530+Gavinfornever@users.noreply.github.com>
2026-06-19 01:06:42 -0700

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) == 0:return 0
+        dic1 = defaultdict(int)
+        dic2 = defaultdict(int)
+        minNum = float('inf')
+        maxNum = float('-inf')
+        for x in nums:
+            dic1[x] = dic1.get(x,0)+1
+            minNum = min(minNum, x)
+            maxNum = max(maxNum, x)
+        print(dic1)
+        p = minNum + 1
+        dic2[minNum] = 1
+        while p<=maxNum:
+            if p not in dic1:
+                p+=1
+                continue
+            if (p-1 not in dic2):
+                dic2[p] = dic2.get(p,0)+1
+            else:
+                dic2[p] = dic2[p-1]+1
+            print(p, dic2[p])
+            p+=1
+        
+        return max(dic2.values())
