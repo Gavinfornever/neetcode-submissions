cbb1f292d9960c6dfbfb24971b1e01a471b48e90
Gavinfornever <100469530+Gavinfornever@users.noreply.github.com>
2026-06-27 13:56:58 -0700

    Add: validate-parentheses - submission-16

---
+++ Data Structures & Algorithms/validate-parentheses/submission-16.py
@@ -0,0 +1,26 @@
+from collections import deque
+class Solution:
+    def isValid(self, s: str) -> bool:
+        length = len(s)
+        if length%2!=0:return False
+
+        dic = {
+            # '[':']',
+            ']':'[',
+            # '(':')',
+            ')':'(',
+            # '{':'}',
+            '}':'{',
+        }
+        q = deque()
+
+        for x in s:
+            if x not in dic.keys():
+                q.append(x)
+            else:
+                if not q:return False
+                tmp = q.pop()
+                if dic[x] != tmp:
+                    return False
+        
+        return True
