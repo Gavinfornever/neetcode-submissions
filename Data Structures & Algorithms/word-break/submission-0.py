ebd621c8860bb5578adf804f5f1c433cd590de1b
Gavinfornever <100469530+Gavinfornever@users.noreply.github.com>
2026-07-07 22:25:27 -0700

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        dp = {}
+        length = len(s)
+        last = 0
+        for i in range(length):
+            if s[:i+1] not in wordDict and \
+                s[last+1:i+1] not in wordDict:
+                dp[i] = False
+                continue
+            
+            dp[i] = True
+            last = i
+        
+        return dp[length-1]
+
+
