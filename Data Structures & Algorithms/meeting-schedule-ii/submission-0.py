874cd74f3c661dc1161f2ededb6c6cfddab51543
Gavinfornever <100469530+Gavinfornever@users.noreply.github.com>
2026-07-13 22:25:39 -0700

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.py
@@ -0,0 +1,48 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        def getRemovedIntervals(itls):
+            removedIntervals = []
+            itls = sorted(itls, key=lambda x:x.start)
+            l, r = 0, 1
+            while r<len(itls):
+                if itls[l].end<=itls[r].start:
+                    l+=1
+                    r+=1
+                    continue
+                else:
+                    if itls[l].end<=itls[r].end:
+                        # remove r
+                        removedIntervals.append(itls[r])
+                        tmp = itls[r]
+                        itls[r] = itls[l]
+                        itls[l] = tmp
+                        l=r
+                        r+=1
+                        continue
+                    else:
+                        #remove l
+                        removedIntervals.append(itls[l])
+                        l=r
+                        r+=1
+                        continue
+            return removedIntervals
+        res = 1
+        # removedIntervals = getRemovedIntervals(intervals)
+        removedIntervals = intervals
+        # res = 1
+        while True:
+            removedIntervals = getRemovedIntervals(removedIntervals)
+            if len(removedIntervals)==0:
+                break
+            res+=1
+        return res
+
+
